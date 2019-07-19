48116c943f0bf0d9477cd0f37293ef07481616a6
junhongliu <junhong.liu@ucarinc.com>
2019-07-19 10:00:50 +0800

    添加测试类，测试用例待设计

--- A_junhongliu/ch01/src/test/java/LineCounterTest.java
+++ A_junhongliu/ch01/src/test/java/LineCounterTest.java
@@ -1,2 +1,32 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 public class LineCounterTest {
+    private Map<Character,Integer> rightMap = new HashMap<Character, Integer>();
+
+    private Map<Character,Integer> resultMap =null;
+
+    private LineCounter lineCounter;
+
+    // 定义测试文件
+    private File testFile = null;
+
+    @Before
+    public void init() {
+        lineCounter = new LineCounter();
+        testFile = new File("src\\test\\resources\\testFile.txt");
+    }
+
+    /**
+     * todo 创建expected用例
+     */
+    @Test
+    public void generateResultMapTest(){
+        lineCounter.countLine(testFile);
+    }
+
 }
