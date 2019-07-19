5568942066137a0c27b1970410c74540a4c6a447
potato1949 <potato1949@gmail.com>
2019-07-19 08:14:04 +0800

    change file Path

--- C_yuyang/ch01/src/test/java/Test.java
+++ C_yuyang/ch01/src/test/java/Test.java
@@ -3,12 +3,14 @@ import com.ucar.train.week1.yu.yang03.Statistics;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import org.junit.*;
 
 public class Test {
     @org.junit.Test
     public void test() {
         Statistics s = new Statistics();
-        String filePath = Statistics.class.getResource("/").getPath() + "TheOldManAndTheSea.txt";
+//        String filePath = Statistics.class.getResource("/").getPath() + "TheOldManAndTheSea.txt";
+        String filePath = "src/main/resources/TheOldManAndTheSea.txt";
         s.statistics(filePath);
         System.out.println(String.format("%-15s", "单词量") + s.sumOfWords);
         System.out.println(String.format("%-15s", "词汇量") + s.minWords);
