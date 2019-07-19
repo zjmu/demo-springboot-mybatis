141a6b6dd8c11da0a33246de5f9bdd8d99d032e8
jinmu.zhou <870855698@qq.com>
2019-07-19 17:27:37 +0800

    初始化

---
+++ B_zhoujinmu/countwords2/src/test/java/com/ucar/train/week2/jinmu/zhou/countwords2/Countwords2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ucar.train.week2.jinmu.zhou.countwords2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Countwords2ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
