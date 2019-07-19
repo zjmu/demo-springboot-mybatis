0a92f60030fd9dd805c361ce1971c2a290f31a72
junhongliu <junhong.liu@ucarinc.com>
2019-07-19 11:12:58 +0800

    实现测试类

--- A_junhongliu/ch01/src/test/java/WordsCounterTest.java
+++ A_junhongliu/ch01/src/test/java/WordsCounterTest.java
@@ -1,2 +1,32 @@
+import org.junit.Before;
+import org.junit.Test;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 public class WordsCounterTest {
+    private Map<Character,Integer> rightMap = new HashMap<Character, Integer>();
+
+    private Map<Character,Integer> resultMap =null;
+
+    private WordsCounter wordsCounter;
+
+    // 定义测试文件
+    private File testFile = null;
+
+    @Before
+    public void init() {
+        wordsCounter = new WordsCounter();
+        testFile = new File("src\\test\\resources\\testFile.txt");
+    }
+
+    /**
+     * 暂时只是输出数据
+     * todo 创建expected用例进行自动测试
+     *
+     */
+    @Test
+    public void generateResultMapTest(){
+        wordsCounter.countWords(testFile);
+    }
 }
