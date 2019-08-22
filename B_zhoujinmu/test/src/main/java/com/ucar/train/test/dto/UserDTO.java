7ed4b5e765744a2c51e7718c983e8582f1289976
jinmu.zhou <870855698@qq.com>
2019-08-22 17:09:51 +0800

    demo

--- B_zhoujinmu/test/src/main/java/com/ucar/train/test/dto/UserDTO.java
+++ B_zhoujinmu/test/src/main/java/com/ucar/train/test/dto/UserDTO.java
@@ -1,5 +1,7 @@
 package com.ucar.train.test.dto;
 
+import java.io.Serializable;
+
 /**
  * @author zhoujinmu
  * @title UserDTO
@@ -7,5 +9,44 @@ package com.ucar.train.test.dto;
  * @description TODO
  * @created 2019-08-07 09:14
  **/
-public class UserDTO {
+public class UserDTO implements Serializable {
+    private Long id;
+    private String name;
+    private String psw;
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", psw='" + psw + '\'' +
+                '}';
+    }
+
+    public UserDTO() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPsw() {
+        return psw;
+    }
+
+    public void setPsw(String psw) {
+        this.psw = psw;
+    }
 }
