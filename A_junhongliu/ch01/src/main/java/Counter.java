664c8580d78185356854e9db40fff9e38edf9551
jinmu.zhou <870855698@qq.com>
2019-08-28 11:08:16 +0800

    demo

--- A_junhongliu/ch01/src/main/java/Counter.java
+++
@@ -1,6 +0,0 @@
-public class Counter {
-    protected String ioMethod = null;
-    public void chooseIoMethod(String ioMethod){
-        this.ioMethod = ioMethod;
-    }
-}
--- B_wufuqiang/ch01/src/main/java/com/ucar/train/fuqiang/wu/ch01/ArticleIndicator.java
+++
@@ -1,6 +0,0 @@
-package com.ucar.train.fuqiang.wu.ch01;
-
-public class ArticleIndicator {
-
-
-}
