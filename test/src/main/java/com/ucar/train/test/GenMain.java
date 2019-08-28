64f009bbbfe284501b8a523ccebf41196b27bc3d
jinmu.zhou <870855698@qq.com>
2019-08-28 11:11:09 +0800

    demo1

---
+++ test/src/main/java/com/ucar/train/test/GenMain.java
@@ -0,0 +1,56 @@
+package com.ucar.train.test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+import org.springframework.util.ResourceUtils;
+/**************************************
+ * 类说明:
+ *     mybatis逆向工程main函数
+ ***************************************
+ */
+public class GenMain {
+    public static void main(String[] args) throws FileNotFoundException {
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        //如果这里出现空指针，直接写绝对路径即可。
+        String genCfg = "D:\\案例项目\\mybatis反向工程\\countwords2\\B_zhoujinmu\\test\\src\\main\\resources\\mybatis-generator.xml";
+//        File configFile = new File(GenMain.class.getResource(genCfg).getFile()); //获取路径出错
+        File configFile = ResourceUtils.getFile(genCfg);
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = null;
+        try {
+            config = cp.parseConfiguration(configFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            e.printStackTrace();
+        }
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = null;
+        try {
+            myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+        try {
+            myBatisGenerator.generate(null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
